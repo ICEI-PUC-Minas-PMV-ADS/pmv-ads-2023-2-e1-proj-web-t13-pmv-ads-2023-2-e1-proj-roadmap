3d62cece1848c9fd087ebf4717bad3722563809a
lmatsuii <leonardomatsuii@gmail.com>
2023-11-21 21:39:23 -0300

    feat: Adicionado exemplo com modal

--- codigo-fonte/assets/js/example.js
+++ codigo-fonte/assets/js/example.js
@@ -9,90 +9,43 @@
  */
 
 
-// Selecionado o elemento HTML onde eu quero inserir todo o conteúdo dinamico
-const divContent = document.getElementById('content');
+
+/*************** */
 
 /**
- * Definido o conteúdo HTML que será replicado N vezes e será inserido dinamicamente 
- * O que for dinamico deve ser escrito simulando o comportamento de uma variavel, por exemplo {title}
- * Mas ela ainda se trata de uma string, apenas sera utilizado como parametro de busca para substituir o conteúdo
- * Nesse cenário temos duas "váriaveis":
- * {title}
- * {body}
- * Poderiam ter outras como como de classes, ids, etc
+ * Modal
  */
-
-let htmlTemplate = `
-<article data-step="1">
-    <header class="d-flex align-items-center fundo_verde ">
-    <i class="fa-solid fundo_verde"></i>
-    <h6 class="text-center text-uppercase text-black my-3 ps-4">
-        {title}
-    </h6>
-    </header>
-
-    <div class="body">
-    <small>
-        {body}
-    </small>
-
-    <div class="d-flex justify-content-between mt-3 controls">
-        <a class="btn btn-outline-dark invisible" href="#">Voltar</a>
-        <a class="btn btn-outline-dark" href="#">Proximo</a>
-    </div>
-    </div>
-</article>
-`
-
-/**
- * Definido meu array de objetos com o conteúdo dinamico
- */
-let content = [
-    {
-        "title": 'Titulo 1',
-        "body": 'Conteudo 1'
-    },
-    {
-        "title": 'Titulo 2',
-        "body": 'Conteudo 2'
-    },
-    {
-        "title": 'Titulo 3',
-        "body": 'Conteudo 3'
-    },
-    {
-        "title": 'Titulo 4',
-        "body": 'Conteudo 4'
-    },
-    {
-        "title": 'Titulo 5',
-        "body": 'Conteudo 5'
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data')
+    let title = '';
+    let content = '';
+    
+    switch ( recipient ) {
+      case "1":
+        title = 'Titulo 1';
+        content = 'Conteudo 1'
+        break;
+      case "2":
+        title = 'Titulo 2';
+        content = 'Conteudo 2';
+        break;
     }
-];
 
-/**
- * Definido váriaveis auxiliares para a montagem do HTML
- */
-let htmlContent = [];
-let replaceTitle = replaceBody = '';
+  
+    
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
 
-/**
- * Iterado o array de conteúdo
- * Cada "variavel" será substituida pela respectiva propriedade do objeto
- * Ao final teremos um array de strings com os conteudos HTML
- */
-content.forEach(content => {
-    replaceTitle = htmlTemplate.replace('{title}', content.title);
-    replaceBody = replaceTitle.replace('{body}', content.body);
-    htmlContent.push(
-        replaceBody
-    );
-});
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body')
 
-/**
- * Iterado o array de strings HTML
- * Adicionado no elemento HTML que foi selecionado anteriormente
- */
-htmlContent.forEach( content => {
-    divContent.insertAdjacentHTML("beforeend", content);
-});
+    modalTitle.textContent = title;
+    modalBodyInput.textContent = content;
+  })
+}
