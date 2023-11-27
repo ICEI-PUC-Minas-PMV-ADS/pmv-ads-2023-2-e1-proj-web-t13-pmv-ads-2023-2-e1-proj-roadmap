54a7b07cbfc3a9dde38db053c597a33b68c02766
Thais Bertini <thaisbertini.ux@hotmail.com>
2023-11-26 22:49:51 -0300

    Inclusão dos botões saiba mais no devops e no AI

--- codigo-fonte/assets/js/intelig-artificial.js
+++ codigo-fonte/assets/js/intelig-artificial.js
@@ -27,6 +27,8 @@ if (exampleModal) {
     let content = "";
     let listItens = [];
     
+    let botaoSaibaMais = document.getElementById('linkSaibaMais')
+    let saibaMaisLink;
 
     switch (recipient) {
       case "1":
@@ -42,6 +44,7 @@ if (exampleModal) {
           'Conhecer a biblioteca NumPy',
           "Conhecer a biblioteca Pandas",
         ];
+        saibaMaisLink = 'https://medium.com/data-hackers/primeiros-passos-em-data-science-utilizando-python-para-an%C3%A1lise-de-dados-823436432b28'
         break;
       case "2":
         modalAtual =title = "Python - Coleções";
@@ -52,11 +55,11 @@ if (exampleModal) {
           'Utilizar polimorfismo nas coleções',
           "Utilizar conjuntos e dicionários",
         ];
+        saibaMaisLink = 'https://medium.com/@desk467/cole%C3%A7%C3%B5es-em-python-tuplas-4444d683963a'
         break;
       case "3":
         modalAtual = title = "Estatística e Matemática - Fundamentos";
-        content =
-          "";
+        content =  "";
         listItens = [
           "Equações, funções e limites",
           'Logaritmos',
@@ -71,6 +74,7 @@ if (exampleModal) {
           'Regressão linear',
           "Séries temporais",
         ];
+        saibaMaisLink = 'https://beatrizyumi.medium.com/preciso-saber-matem%C3%A1tica-para-aprender-ci%C3%AAncia-de-dados-9a9b7c7846a9'
         break;
       case "4":
         modalAtual = title = "Machine Learning - Fundamentos";
@@ -81,6 +85,7 @@ if (exampleModal) {
           'Utilizar algoritmos de regressão',
           'Utilizar o Scikit-learn para criar modelos de machine learning',
         ];
+        saibaMaisLink = 'https://kozyrk.medium.com/portuguese-unsupervised-learning-c47d48c8e31'
         break;
       case "5":
         modalAtual = title = "Machine Learning - Aprendizado Não Supervisionado";
@@ -93,7 +98,8 @@ if (exampleModal) {
           'Parametrizar métodos de clusterização através do máximo coeficiente de silhueta',
           'Entender a matemática por trás das métricas de validação Silhouette, Davies Bouldin e Calinski Harabasz',
           "Conhecer técnicas de redução de dimensionalidade",
-        ];        
+        ]; 
+        saibaMaisLink = 'https://kozyrk.medium.com/portuguese-unsupervised-learning-c47d48c8e31'       
         break;
       case "6":
         modalAtual = title = "Regressão Linear e Logística";
@@ -103,27 +109,31 @@ if (exampleModal) {
           'O objetivo é encontrar uma relação linear que melhor se ajuste aos dados.',
           'A regressão logística, por outro lado, é usada para problemas de classificação binária, onde a variável de saída é categórica e possui apenas duas categorias. Ela estima a probabilidade de um evento ocorrer com base nas variáveis independentes.',
         ] 
-
+        saibaMaisLink = 'https://techguide.sh/pt-BR/path/inteligencia-artificial/linear-and-logistic-regression/'
         break;
       case "7":
         modalAtual = title = "Previsão de Séries temporais";
         content = "A previsão de séries temporais (Time series Forecasting), também conhecida como previsão temporal, é uma técnica de análise estatística que envolve a previsão de valores futuros ou padrões com base em dados históricos ordenados no tempo. Em uma série temporal, os dados são coletados sequencialmente em intervalos regulares, como horários, diários, mensais ou anuais, e exibem dependências temporais.";
         listItens = [] 
+        saibaMaisLink = 'https://towardsdatascience.com/the-complete-guide-to-time-series-forecasting-using-sklearn-pandas-and-numpy-7694c90e45c1'
         break;
       case "8":
         modalAtual = title = "Redução de Dimensionalidade";
         content = "Redução de dimensionalidade é uma técnica utilizada no campo de aprendizado de máquina e análise de dados para reduzir o número de variáveis ou dimensões em um conjunto de dados. O objetivo é simplificar a representação dos dados, eliminando ou combinando características redundantes ou irrelevantes, enquanto mantém a maior parte da informação original.";
         listItens = [] 
+        saibaMaisLink = 'https://www.aprendadatascience.com/blog/an%C3%A1lise-explorat%C3%B3ria-de-dados/redu%C3%A7%C3%A3o-de-dimensionalidade'
         break;
       case "9":
         modalAtual = title = "Machine Learning - Aprendizado por Transferência";
         content = "Transfer Learning (Aprendizado por Transferência) é uma abordagem na área de Machine Learning em que um modelo pré-treinado em uma tarefa relacionada é reutilizado como ponto de partida para treinar um modelo em uma tarefa diferente, geralmente com menos dados disponíveis.";
         listItens = [] 
+        saibaMaisLink = 'https://datascience.eu/pt/aprendizado-de-maquina/o-que-e-aprendizado-por-transferencia/'
         break;
       case "10":
         modalAtual = title = "Machine Learning - Regularização";
         content = "Regularização se refere a um conjunto de técnicas para tornar os dados mais regulares e estáveis, facilitando o aprendizado e a descoberta de padrões em Machine Learning. A regularização é uma técnica que reduz o erro de um modelo, evitando o sobreajustamento (Overfitting) e treinando o modelo para funcionar corretamente.";
         listItens = [] 
+        saibaMaisLink = 'https://datascience.eu/pt/aprendizado-de-maquina/regularizacao-na-aprendizagem-automatica/'
         break;
       case "11":
         modalAtual = title = "Visão Computacional";
@@ -137,11 +147,13 @@ if (exampleModal) {
           'Conhecer Redes Neurais Convolucionais',
           'Usar OpenCV',
           ] 
+          saibaMaisLink = 'https://medium.com/turing-talks/introdu%C3%A7%C3%A3o-%C3%A0-vis%C3%A3o-computacional-b13698774adc'
         break;
       case "12":
         modalAtual = title = "Redes Neurais Convolucionais";
         content = "As CNNs (Convolutional Neural Networks, ou Redes Neurais Convolucionais) são um subtipo de redes neurais artificiais profundas (Deep Learning). AS CNNs são majoritariamente empregadas para o processamento de imagens e vídeos, no contexto de classificação do seu conteúdo. Ao contrário de outros algoritmos tradicionais de classificação de imagens, uma CNN tende a demandar menos esforços de pré-processamento nos dados de entrada.";
         listItens = [] 
+        saibaMaisLink = 'https://www.quanta.org.br/blog/reconhecimento-de-imagens-com-redes-neurais/?gclid=EAIaIQobChMIyPalv5W4-gIVSeZcCh1SFQPeEAAYAyAAEgLZxPD_BwE'
         break;
       case "13":
         modalAtual = title = "Processamento de Linguagem Natural";
@@ -158,6 +170,7 @@ if (exampleModal) {
           'Conhecer o Word2Vec',
           "Combinar vetores de palavras para representar textos e classificá-los",
           ]
+          saibaMaisLink = 'https://medium.com/ensina-ai/introdu%C3%A7%C3%A3o-a-processamento-de-linguagem-natural-174936c096b'
         break;
       case "14":
         modalAtual = title = "Redes Neurais Recorrentes";
@@ -168,26 +181,31 @@ if (exampleModal) {
           'Funções de ativação mais comuns',
           "Familiarizar-se com as principais arquiteturas de RNNs, como bidirecionais (BRNN), Memória de curto prazo longa (LSTM), Unidades recorrentes fechadas (GRU)",
           ] 
+          saibaMaisLink = 'https://www.ibm.com/br-pt/topics/recurrent-neural-networks'
         break;
       case "15":
         modalAtual = title = "Redes Adversárias Generativas (GANs)";
         content = "Redes Adversárias Generativas (em inglês, Generative Adversarial Networks ou GANs) são um tipo de arquitetura de rede neural usada para gerar dados sintéticos, como imagens, música, texto ou outros tipos de dados, que se assemelham aos dados de treinamento.";
         listItens = [] 
+        saibaMaisLink = 'https://www.deeplearningbook.com.br/introducao-as-redes-adversarias-generativas-gans-generative-adversarial-networks/'
         break;
       case "16":
         modalAtual = title = "Automação com IA";
         content = "Automated IA, ou automação de IA, refere-se à combinação de tecnologias de automação, como RPA, com recursos de inteligência artificial, como processamento de linguagem natural (NLP), aprendizado de máquina (machine learning) e visão computacional. Essa abordagem visa automatizar processos complexos que envolvem o processamento e a análise de informações não estruturadas, permitindo que as máquinas tomem decisões com base nesses dados.";
         listItens = [] 
+        saibaMaisLink = 'https://www.automationanywhere.com/br/rpa/intelligent-automation'
         break;
       case "17":
         modalAtual = title = "XGBoost";
         content = "XGBoost é uma biblioteca de aprendizado de máquina baseada em gradient boosting que se destaca na resolução de problemas de classificação e regressão. 'XG' em XGBoost refere-se a 'Extreme Gradient', indicando o uso de gradient boosting para melhorar a precisão do modelo.";
         listItens = [] 
+        saibaMaisLink = ''
         break;
       case "18":
         modalAtual = title = "Viés Cognitivo e IA Responsável";
         content = "O conceito de Viés Cognitivo refere-se aos padrões sistemáticos de desvio e distorção na forma como os seres humanos percebem, interpretam e tomam decisões. Esses vieses cognitivos podem levar a distorções na análise de informações, julgamentos e comportamentos de todos os tipos, como de raça, gênero, idade, capacidades físicas, entre outros, influenciando nossas percepções e ações de maneira não racional ou imparcial.";
         listItens = [] 
+        saibaMaisLink = ''
         break;
 
     }
@@ -221,6 +239,7 @@ if (exampleModal) {
     modalBodyInput.textContent = content;
 
     document.getElementById("contentList").appendChild(ul);
+    botaoSaibaMais.innerHTML = ` <a href="${saibaMaisLink}" target="__blank">Saiba mais</a>`
   });
 }
 
